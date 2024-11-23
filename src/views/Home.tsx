b8712e4803117c90b6223658dd46718f6e89ac22
mmc003 <mingyangmchu@gmail.com>
2024-11-23 14:20:12 -0800

    nov 23 imgs up

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
+import "./Home.css"
+
+import homepage from "../imgs/9684-1_07.jpg"
 
 const Home: React.FC = () => {
-  return <div className='subtitle'>Home</div>;
+  return (
+    <div>
+      <div className="subtitle">Home</div>
+      <img src={homepage} alt="" className="homepage-img" />
+    </div>
+  );
 };
 
 export default Home;
--- src/views/Recents.tsx
+++ src/views/Recents.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import "./Recents.css";
 
-import wkw from "../imgs/wongkarwai-watch-order.jpg";
+import img1 from "../imgs/9684-1_03.jpg";
+import img2 from "../imgs/9684-1_07.jpg";
+import img3 from "../imgs/9684-1_06.jpg";
+import img4 from "../imgs/9684-1_10.jpg";
+import img5 from "../imgs/9684-1_17.jpg";
+import img6 from "../imgs/9684-1_20.jpg";
+import img7 from "../imgs/9684-1_21.jpg";
+
+
 
 const Recents: React.FC = () => {
-  // const imgs = [wkw];
+  const imgs = [img1, img2, img3, img4,  img6, img5,img7];
   return (
     <div>
       <div className="subtitle">Recent Work</div>
-      {/* <div className="gallery-grid">
+      <div className="gallery-grid">
         {imgs.map((image, index) => (
           <img
             key={index}
@@ -17,7 +25,7 @@ const Recents: React.FC = () => {
             className="gallery-image"
           />
         ))}
-      </div> */}
+      </div>
     </div>
   );
 };
